fb2e0c1e386d3473fb8a50125a83a92523ac3854
VAbrishamii <vahide.abri@yahoo.com>
2025-04-18 18:22:07 +0200

    update loadmorebutton

--- holidaze/src/component/ui/LoadMoreButton.tsx
+++ holidaze/src/component/ui/LoadMoreButton.tsx
@@ -13,7 +13,7 @@ export default function LoadMoreButton({
     return (
         <button
         {...props}
-        className={`flex items-center justify-center  px-4 py-2 text-white bg-[var(--color-darkgreen)] rounded-md  focus:outline-none focus:ring-2  cursor-pointer focus:ring-opacity-50 ${
+        className={`px-4 py-2 text-white bg-[var(--color-darkgreen)] rounded-md  focus:outline-none focus:ring-2  cursor-pointer focus:ring-opacity-50 ${
             isLoading ? "cursor-not-allowed" : ""
         }`}
         disabled={isLoading}
--- holidaze/src/component/venues/VenueList.tsx
+++ holidaze/src/component/venues/VenueList.tsx
@@ -51,7 +51,7 @@ export default function VenueList() {
 
   return (
     <div className="space-y-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         {venues.map((venue) => (
           <VenueCard key={venue.id} venue={venue} />
         ))}

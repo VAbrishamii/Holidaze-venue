3c437dd22a58379eb93d92d2e2f8d514cd98f2f5
VAbrishamii <vahide.abri@yahoo.com>
2025-05-14 17:03:39 +0200

    Update page.tsx

--- holidaze/src/app/auth/profile/create/page.tsx
+++ holidaze/src/app/auth/profile/create/page.tsx
@@ -1,14 +1,17 @@
+"use client";
 import AmenitiesSelector from "@/component/ui/AmenitiesSelector";
 
 export default function CreateVenue() {
-    return (
-        <div className="p-4 max-w-5xl mx-auto">
-            <h1 className="text-2xl font-bold mb-4">Create Venue</h1>
-            <form>
-                {/* Add form fields here */}
-              
-                
-            </form>
-        </div>
-    );
+  return (
+    <div className="p-4 max-w-5xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Create Venue</h1>
+      <form>
+        {/* Add form fields here */}
+        <AmenitiesSelector
+          value={{ wifi: false, breakfast: false, parking: false, pets: false }}
+          onChange={(value) => console.log(value)}
+        />
+      </form>
+    </div>
+  );
 }

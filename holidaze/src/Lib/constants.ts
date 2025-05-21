5b5e9b54c87e3e903662884b4413ee30bedd6234
VAbrishamii <vahide.abri@yahoo.com>
2025-05-21 14:10:20 +0200

    Update constants.ts

--- holidaze/src/Lib/constants.ts
+++ holidaze/src/Lib/constants.ts
@@ -6,3 +6,4 @@
 export const API_BASE = process.env.NEXT_PUBLIC_API_BASE!; 
 console.log('base api', API_BASE);
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY!;
+console.log('api key', API_KEY);

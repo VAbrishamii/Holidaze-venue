0e383c31ce084b548d071fe19d409d07511532c7
VAbrishamii <vahide.abri@yahoo.com>
2025-05-22 12:26:39 +0200

    update ReadME

--- holidaze/src/Lib/constants.ts
+++ holidaze/src/Lib/constants.ts
@@ -3,6 +3,10 @@
  * API base URL from environment
  
  */
+if (!process.env.NEXT_PUBLIC_API_BASE || !process.env.NEXT_PUBLIC_API_KEY) {
+  throw new Error("Missing environment variables. Did you forget to add .env.local?");
+}
+
 export const API_BASE = process.env.NEXT_PUBLIC_API_BASE!; 
 console.log('base api', API_BASE);
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY!;

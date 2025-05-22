42ce6853434dfac8cd5742711bd3e10ed11a9ea7
VAbrishamii <vahide.abri@yahoo.com>
2025-05-22 15:55:17 +0200

    Create useSearchStore.ts

---
+++ holidaze/src/hooks/useSearchStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { Venue } from "@/Lib/types/venue";
+
+type SearchStatus = "idle" | "pending" | "success" | "error";
+/**
+ * SearchStore interface to manage search results and status
+ * - results: Array of Venue objects or null
+ */
+type SearchStore = {
+  results: Venue[] | null;
+  status: SearchStatus;
+  setResults: (results: Venue[] | null) => void;
+  setStatus: (status: SearchStatus) => void;
+  reset: () => void;
+};
+/**
+ * useSearchStore hook to manage search results and status using Zustand
+ * - setResults: Function to set search results
+ * - setStatus: Function to set search status
+ * - reset: Function to reset search store
+ */
+export const useSearchStore = create<SearchStore>((set) => ({
+  results: null,
+  status: "idle",
+  setResults: (results) => set({ results }),
+  setStatus: (status) => set({ status }),
+  reset: () => set({ results: null, status: "idle" }),
+}));
